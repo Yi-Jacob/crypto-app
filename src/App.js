c5c14778523e390cd231b6700ea25e5d887eeaa8
Yi-Jacob <jakeyi00@gmail.com>
2022-09-25 20:45:51 -0700

    topbanner update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import TopBanner from "./TopBanner";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 
 function App() {
@@ -30,25 +31,46 @@ function App() {
       <Row>
         <Col md={12} className="text-center">
           <TopBanner />
-          <h1>Top 100 Cryptocurrencies</h1>
-          <input
-            type="text"
-            placeholder="Search..."
-            onChange={(e) => {
-              setSearch(e.target.value);
-            }}
-          />
+
           </Col>
         <Col md={9}>
           <div className="App">
-            {/* <h1>Top 100 Cryptocurrencies</h1>
+            <h1>Top 100 Cryptocurrencies</h1>
             <input
               type="text"
               placeholder="Search..."
               onChange={(e) => {
                 setSearch(e.target.value);
               }}
-            /> */}
+            />
+            <Row>
+
+
+
+            {crypto
+              .filter((val) => {
+                return val.name.toLowerCase().includes(search.toLowerCase());
+              })
+              .map((val, id) => {
+                return (
+                  <>
+                    <Col md={4}>
+                      <Card style={{ margin: '12px auto' }}>
+                        <Card.Img className="logo-img" width="30px" variant="top" src={val.icon} />
+                        <Card.Body>
+                          <Card.Title className="text-center">{val.name}</Card.Title>
+                          <Card.Text className="text-center">Rank: {val.rank} || Ticker: {val.symbol}</Card.Text>
+                          <Card.Title className="text-center">${val.price.toFixed(2)}</Card.Title>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  </>
+                );
+              })}
+
+                </Row>
+
+
             <table>
               <thead>
                 <tr>
@@ -77,7 +99,7 @@ function App() {
                             <a href={val.websiteUrl}>
                               <img src={val.icon} alt="logo" width="30px" />
                             </a>
-                            <p>{val.name}</p>
+                            <p className="overflow-hidden">{val.name}</p>
                           </td>
                           <td className="symbol">{val.symbol}</td>
                           <td>${val.marketCap.toLocaleString()}</td>
